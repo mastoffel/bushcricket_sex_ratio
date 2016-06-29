99579a5852e5112a06f3586c57663b3a5629c8b3
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-06-29 23:11:57 +0200

    some exploratory analyses added

--- R/exploration.R
+++ R/exploration.R
@@ -1,25 +1,164 @@
 library(dplyr)
 
-# load data
-census <- read.csv("data/raw/census_data.csv")
-morphometrics <- read.csv("data/raw/morphometric_data.csv")
+# morphometrics data
+morphometrics <- read.csv("data/raw/morphometric_data.csv") %>% 
+                        mutate(popid = paste0(Population, ID)) %>% # creates unique id
+                        mutate(Sex = as.character(Sex))
+# load census 
+census <- read.csv("data/raw/census_data.csv") %>% 
+                        rename(session_day = Date_of_1st_session) %>% 
+                        mutate(popid = paste0(Population, ID)) %>% 
+                        mutate(sex_ratio = as.factor(ifelse(Population == "A" | Population == "E" | Population == "G", 0.5,
+                        ifelse(Population == "B" | Population == "F" | Population == "H", 0.75, 0.25))))# creates unique id
+                        census[is.na(census$ID), "popid"] <- NA
+                        
+# fill in sex variable
+fill_in_sex <- function(x){
+    if (x["Sex"] == "") {
+        if (x["popid"] %in% (morphometrics$popid)){
+        rownum <- which(morphometrics$popid == x["popid"])
+        x["Sex"] <- morphometrics[rownum, "Sex"]
+        }
+    }
+    x
+}
 
-# load mating and add treatment
+census <- as.data.frame(t(apply(census, 1, fill_in_sex)))
+
+# load mating and add treatment factor
 mating <- read.csv("data/raw/mating_data.csv", stringsAsFactors = FALSE, na.strings = "") %>% 
-    mutate(sex_ratio = as.factor(ifelse(Population == "A" | Population == "E" | Population == "G", 0.5,
-        ifelse(Population == "B" | Population == "F" | Population == "H", 0.75, 0.25))))
+                                 mutate(sex_ratio = as.factor(ifelse(Population == "A" | Population == "E" | Population == "G", 0.5,
+                                 ifelse(Population == "B" | Population == "F" | Population == "H", 0.75, 0.25)))) %>%
+                                 rename(session_day = Day_of_1st_session) 
 
+# transform all variable names to lowercase
+out <- lapply(list(census, mating, morphometrics), function(x){
+    names(x) <- tolower(names(x))
+    x
+}) 
 
-str(mating)
+# reassign to dataframes
+census <- out[[1]]
+mating <- out[[2]]
+morphometrics <- out[[3]]
 
-mating2 <- mating %>% 
-                group_by(sex_ratio, Population, Day_of_1st_session) %>%
-                summarise(matings = sum(Mating, na.rm = TRUE)) 
+# transform variables to factor
+mating <- mating %>% mutate(population = as.factor(population))
+
+mating_over_time <- mating %>% 
+                group_by(sex_ratio, session_day, population) %>%
+                summarise(matings = sum(mating, na.rm = TRUE)) 
+
+means <- aggregate(matings ~ sex_ratio, mating_over_time, mean)
+
+library(ggthemes)
+library(ggplot2)
+library(ggrepel)
+
+d <- ggplot(mating_over_time, aes(x = sex_ratio, y = matings)) 
+d + geom_boxplot(outlier.shape = NA, lwd = 0.5) + 
+    geom_jitter(aes(color = population), alpha = 0.7, size = 2) +
+    scale_y_continuous(breaks=0:9) +
+    #stat_summary(fun.y=mean, colour="red", geom="point", 
+    #     size=3) +
+    xlab("sex ratio") +
+    ylab("number of matings per night") +
+    theme_classic() +
+    theme(axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.title.y=element_text(margin=margin(0,20,0,0)),
+        axis.title.x=element_text(margin=margin(20,0,0,0)))
+
+   # geom_text(data = means, aes(label = matings, y = matings - 0.5))
+just_mating <- mating %>% filter(mating == 1) %>%
+                mutate(time_of_day = ifelse(time > 1000, "21-24", (ifelse((time > 0 & time < 300), "0-3", "3-6"))))
+
+just_mating$time_of_day <- as.factor(just_mating$time_of_day )
+just_mating$time_of_day <- factor(just_mating$time_of_day ,levels(just_mating$time_of_day )[c(2,1,3)])
+
+d <- ggplot(just_mating, aes(sex_ratio)) 
+d + geom_bar(aes(fill=time_of_day)) +
+    xlab("sex ratio") +
+    ylab("overall number of matings") +
+    theme_classic() +
+    theme(axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.title.y=element_text(margin=margin(0,20,0,0)),
+        axis.title.x=element_text(margin=margin(20,0,0,0)))
+
 
 library(ggplot2)
-d <- ggplot(mating2, aes(as.factor(Day_of_1st_session), matings, colour = sex_ratio)) + geom_point()
-d + stat_summary(fun.data = "mean_cl_boot", size = 1)
+d <- ggplot(mating_over_time, aes(session_day, matings, colour = sex_ratio)) + 
+     stat_summary(fun.y = "sum", size = 0.7, geom = "line") +
+    stat_summary(fun.y = "sum", size = 1.8, geom = "point") +
+    scale_x_continuous(breaks=6:18) +
+    scale_y_continuous(breaks=0:12) +
+    xlab("session date") +
+    ylab("number of matings") +
+    theme_classic() +
+    theme(axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.title.y=element_text(margin=margin(0,20,0,0)),
+        axis.title.x=element_text(margin=margin(20,0,0,0)))
+d
 
-mating3 <-  mating %>% 
-    group_by(sex_ratio) %>%
-    summarise(matings = sum(Mating, na.rm = TRUE)) 
+# census data
+# census <- census %>% mutate(session_day = as.factor(session_day))
+census_summary <- census %>% 
+                    mutate(session_day = as.numeric(as.character(session_day))) %>%
+                    filter(is.na(immigrant) | is.na(pre_census)) %>%
+                    group_by(session_day, sex_ratio, sex) %>%
+                    summarise(num_ind = length(id)) %>%
+                    filter(!(is.na(session_day))) %>%
+                    filter(sex == "F" | sex == "M") %>%
+                    mutate(num_ind == as.numeric(as.character(sex_ratio)))
+              
+# absolute sex-specific dispersal
+labels <- c(F = "Females", M = "Males")
+sp + facet_grid(. ~ sex, labeller=labeller(sex = labels))
+
+d <- ggplot(census_summary, aes(session_day, num_ind, colour = sex_ratio)) + 
+     geom_point(size = 1.8) + 
+     stat_summary(fun.y = "sum", size = 0.7, geom = "line") +
+     facet_wrap(~ sex, labeller=labeller(sex = labels)) +
+    scale_x_continuous(breaks=seq(from = 6, to = 18, by = 2)) +
+    scale_y_continuous(breaks=seq(from = 0, to = 90, by = 10)) +
+    xlab("census date") +
+    ylab("number of remaining individuals") +
+    theme_classic() +
+    theme(axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.title.y=element_text(margin=margin(0,20,0,0)),
+        axis.title.x=element_text(margin=margin(20,0,0,0)),
+        strip.text.x = element_text(size=13, face="bold" ),
+        strip.background = element_rect(colour="white"),
+        panel.margin = unit(1.5, "lines"))
+d
+
+# relative sex-specific_dispersal
+census_summary$rel_num_ind <- NA
+for (sex in c("M", "F")) {
+    for (i in c(0.25, 0.5, 0.75)) {
+        full_num_ind <- sum(morphometrics$sex == sex & morphometrics$treatment == i)
+        census_summary[census_summary$sex_ratio == i & census_summary$sex == sex, "rel_num_ind"] <- 
+            census_summary[census_summary$sex_ratio == i & census_summary$sex == sex, "num_ind"] / full_num_ind
+    }
+}
+
+d <- ggplot(census_summary, aes(session_day, rel_num_ind, colour = sex_ratio)) +  
+    geom_point(size = 1.8) + 
+    stat_summary(fun.y = "sum", size = 0.7, geom = "line") +
+    facet_wrap(~ sex, labeller=labeller(sex = labels)) +
+    scale_x_continuous(breaks=seq(from = 6, to = 18, by = 2)) +
+    #scale_y_continuous(breaks=seq(from = 0, to = 90, by = 10)) +
+    xlab("census date") +
+    ylab("proportion of remaining individuals") +
+    theme_classic() +
+    theme(axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid'),
+        axis.title.y=element_text(margin=margin(0,20,0,0)),
+        axis.title.x=element_text(margin=margin(20,0,0,0)),
+        strip.text.x = element_text(size=13, face="bold" ),
+        strip.background = element_rect(colour="white"),
+        panel.margin = unit(1.5, "lines"))
+d
